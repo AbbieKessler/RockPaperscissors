a7f1bb4f241a5a3e767f2d7a6ab29dde7f38bf2e
Kessler <25KesslerA@badinhs.org>
2024-10-02 14:25:58 -0400

    step one complete; started on step two

--- RockPaperscissors.js
+++ RockPaperscissors.js
@@ -1,1 +1,34 @@
 // This is a file to program a playable game of rps
+
+// Step 1:
+// test that your javascript file is linked to your HTML file by console.log("Hello, world!");
+//console.log("hello world");   //works
+
+// Step 2: 
+// Create a function called getComputerChoice
+// -getComputerChoice will randomly return "rock", "paper", "scissors"
+// -HINT: Math.random() is a function that returns a number between 0 and 1.  You can use this to make random selections.
+// -Once you think you've got this working, test it by console.log(getComputerChoice());
+function getcomputerChoice() {
+   return  
+}
+
+
+// Step 3:
+// Create a function called getHumanChoice:
+// -Write the code so getHumanChoice returns one of "rock", "paper", or "scissors" based on user input
+// -HINT: prompt() is a function that will allow you to get user input
+// -Once you think you've got this working, test it by console.log(getHumanChoice());
+
+// Step 4:
+// Score tracking
+// -Create two variables to score human and computer scores
+// -initialize these variables with a score of 0
+
+// Step 5: 
+// Write the logic to play one game
+// -Create a function called playRound
+// -2 parameters (humanChoice, computerChoice)
+// -make sure that human choice logic is case-insensitive (rock, RoCK, RocK, ROCK should all work equally well)
+// -Should console.log a message like "You win! Rock beats scissors")
+// -increment the human/computer score based on winner
